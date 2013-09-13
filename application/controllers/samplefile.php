639d9f51403f9944ea5e433c95044c86abfaedc3
srinivasu <srinivasu.m@glowtouch.com>
2013-09-13 17:14:56 +0530

    Added new file to the repo by srinivasu..

---
+++ application/controllers/samplefile.php
@@ -0,0 +1,29 @@
+<?php
+/*
+* File created by Srinivasu for testing with GitHub apps..
+* Created on 09/13/2013
+* Location : Applications/Controller/Samplefile.php
+*/
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class SampleFile extends MY_Controller {
+
+    public function __construct() {
+        parent::__construct();
+        $this->template->set_template('home');
+    }
+
+    public function index() {
+        $this->template->write('title', $this->page_title . ' : Home');
+        $this->template->write_view('content', 'frontend/home');
+        $this->template->render();
+    }
+	
+	public function githubexample() {
+		echo "Just sample method"
+	}
+    
+}
+
+?>
